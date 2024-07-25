347797b044e1eecd686bc913f09516f60a4f56e0
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-25 02:47:07 -0700

    read_write.py

--- read_write.py
+++ read_write.py
@@ -1,21 +1,40 @@
-# TO READ A FILE ##
-with open("my_file.txt") as file: # This will automatically close the file after use.
-    # FYI, File can be named anything you want
-    contents = file.read()
-    print(contents)
+####################################################################
+# # TO READ A FILE ##
+# with open("my_file.txt") as file: # This will automatically close the file after use.
+#     # FYI, File can be named anything you want
+#     contents = file.read()
+#     print(contents)
+####################################################################
 
 
-# TO WRITE TO A FILE ##
-# MODE = " "
-# Mode is set to read only by default._
-# "w" will delete everything in the my_file.txt.
-# "a" will append the text in my_file.txt
+####################################################################
+# # READ FROM DIFFERENT FILE PATH ##
+
+# ABSOLUTE PATH
+# with open("/Users/rsharpe/Desktop/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# # RELATIVE PATH ##
+# with open("../../Desktop/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+####################################################################
 
 
-with open("my_file.txt", mode="a") as file:
-    file.write("Any string we want")
-
-# if ___.txt does not exist, Pycharm will create a new file from scratch.
-with open("new_file.txt", mode="a") as file:
-    file.write("Any string we want")
+####################################################################
+# # TO WRITE TO A FILE ##
+# # MODE = " "
+# # Mode is set to read only by default._
+# # "w" will delete everything in the my_file.txt.
+# # "a" will append the text in my_file.txt
+#
+#
+# with open("my_file.txt", mode="a") as file:
+#     file.write("Any string we want")
+#
+# # if ___.txt does not exist, Pycharm will create a new file from scratch.
+# with open("new_file.txt", mode="a") as file:
+#     file.write("Any string we want")
+####################################################################
 
