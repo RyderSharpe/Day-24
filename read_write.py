66a81ce06c2623fabd9c41cebc37ebcf0e71469a
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-24 23:53:14 -0700

    read_write.py

--- read_write.py
+++ read_write.py
@@ -6,12 +6,16 @@ with open("my_file.txt") as file: # This will automatically close the file after
 
 
 # TO WRITE TO A FILE ##
-MODE = " "
-Mode is set to read only by default._
-"w" will delete everything in the my_file.txt.
-"a" will append the text in my_file.txt
+# MODE = " "
+# Mode is set to read only by default._
+# "w" will delete everything in the my_file.txt.
+# "a" will append the text in my_file.txt
 
+
+with open("my_file.txt", mode="a") as file:
+    file.write("Any string we want")
 
 # if ___.txt does not exist, Pycharm will create a new file from scratch.
 with open("new_file.txt", mode="a") as file:
     file.write("Any string we want")
+
