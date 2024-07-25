45c029f6057355b0b50a0ea06f9b89bb418a47dc
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-24 23:45:05 -0700

    Add files via upload

---
+++ read_write.py
@@ -0,0 +1,18 @@
+## TO READ A FILE ##
+# with open("my_file.txt") as file: # This will automatically close the file after use.
+#     # FYI, File can be named anything you want
+#     contents = file.read()
+#     print(contents)
+
+
+## TO WRITE TO A FILE ##
+# MODE = " "
+# Mode is set to read only by default._
+# "w" will delete everything in the my_file.txt.
+# "a" will append the text in my_file.txt
+with open("my_file.txt", mode="a") as file:
+    file.write("Any string we want")
+
+# if ___.txt does not exist, Pycharm will create a new file from scratch.
+with open("new_file.txt", mode="a") as file:
+    file.write("Any string we want")
